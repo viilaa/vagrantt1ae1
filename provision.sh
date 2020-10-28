9f85f1208812590e36ed13963c7dfa4aa65de90e
jviilaa <javior@floridauniversitaria.es>
2020-10-28 13:15:35 +0100

    actv 14

--- provision.sh
+++ provision.sh
@@ -3,4 +3,5 @@ apt-get -y install nginx
 rm -rf /usr/share/nginx/www/
 cp -avr  /vagrant/www /usr/share/nginx/www/ 
 ln -s /vagrant/www /usr/share/nginx/www/
+cp /etc/nginx/sites-enabled/default /vagrant/sites-enabled
 service nginx start
